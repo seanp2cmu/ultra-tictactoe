199ad24141f6b9800639502ab96f4f6ebaffd742
sean2474 <47488868+sean2474@users.noreply.github.com>
2026-02-12 06:09:14 -0500

    fix: setup_cy.py 빌드 경로 수정

--- game/setup_cy.py
+++ game/setup_cy.py
@@ -1,15 +1,20 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
+ext = Extension(
+    "board_cy",  # 패키지명 없이 모듈명만
+    ["board_cy.pyx"],
+    include_dirs=[np.get_include()],
+)
+
 setup(
     ext_modules=cythonize(
-        "board_cy.pyx",
+        [ext],
         compiler_directives={
             'language_level': "3",
             'boundscheck': False,
             'wraparound': False,
         }
     ),
-    include_dirs=[np.get_include()],
 )
